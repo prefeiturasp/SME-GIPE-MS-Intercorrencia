b69d91e59e0fee49a78ef96fe7e5922db59e329a
ollyvergithub <ollyverottoboni@gmail.com>
2025-09-22 09:26:52 -0300

    feat: :truck: Altera rota principal da aplicação para api-intercorrencias/v1/

--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,18 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+BASE = "api-intercorrencias/v1/"  # ou "intercorrencias/api/v1/"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f"{BASE}admin/", admin.site.urls),
+
     # OpenAPI JSON
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(f"{BASE}schema/", SpectacularAPIView.as_view(), name="schema"),
 
     # Swagger / Redoc
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path(f"{BASE}docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(f"{BASE}redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 
-   
+    # App
+    path(f"{BASE}", include("intercorrencias.urls")),
 ]
