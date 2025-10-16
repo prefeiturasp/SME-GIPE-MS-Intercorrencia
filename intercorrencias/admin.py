0a1ad3785dde182d173e40d7f81ff92cad6c3d93
Marcelo Alves <alvesmarcelinho7@gmail.com>
2025-10-16 15:51:55 -0300

    feat(component): :sparkles: Adiciona novos campos ao admin de intercorrencia

--- intercorrencias/admin.py
+++ intercorrencias/admin.py
@@ -12,6 +12,10 @@ class IntercorrenciaAdmin(admin.ModelAdmin):
         "user_username",
         "dre_codigo_eol",
         "sobre_furto_roubo_invasao_depredacao",
+        "status",
+        "get_tipos_ocorrencia",
+        "descricao_ocorrencia",
+        "smart_sampa_situacao",
         "criado_em",
     )
     list_filter = ("sobre_furto_roubo_invasao_depredacao", "dre_codigo_eol")
@@ -19,6 +23,11 @@ class IntercorrenciaAdmin(admin.ModelAdmin):
     readonly_fields = ("uuid", "criado_em", "atualizado_em")
     ordering = ("-criado_em",)
 
+    def get_tipos_ocorrencia(self, obj):
+        """Mostra os tipos de ocorrência como texto no admin."""
+        return ", ".join([t.nome for t in obj.tipos_ocorrencia.all()])
+
+    get_tipos_ocorrencia.short_description = "Tipos de Ocorrência"
 
 @admin.register(TipoOcorrencia)
 class TipoOcorrenciaAdmin(admin.ModelAdmin):
