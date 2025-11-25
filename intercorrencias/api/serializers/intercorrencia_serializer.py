7a69c11ca9a787d42d0f4bc018c3f001e558f93a
ollyvergithub <ollyverottoboni@gmail.com>
2025-11-25 07:47:22 -0300

    refactor: :truck: Move Intercorrencia Dre Serializer e Testes para arquivos separados

--- intercorrencias/api/serializers/intercorrencia_serializer.py
+++ intercorrencias/api/serializers/intercorrencia_serializer.py
@@ -782,27 +782,3 @@ class IntercorrenciaUpdateDiretorCompletoSerializer(IntercorrenciaSerializer):
         return instance    
 
 
-# ===========================
-# SERIALIZERS DA DRE
-# ===========================
-
-class IntercorrenciaDreSerializer(IntercorrenciaSerializer):
-    """Serializer completo para DRE - preenche campos próprios"""
-    
-    class Meta:
-        model = Intercorrencia
-        fields = (
-            # Básicos (read-only para DRE)
-            "id", "uuid", "unidade_codigo_eol", "dre_codigo_eol",
-            
-            # Campos próprios da DRE (editáveis)
-            "acionamento_seguranca_publica", "interlocucao_sts", "info_complementar_sts",
-            "interlocucao_cpca", "info_complementar_cpca", "interlocucao_supervisao_escolar", 
-            "info_complementar_supervisao_escolar", "interlocucao_naapa", "info_complementar_naapa",
-
-        )
-        
-    def validate(self, attrs):
-        return super().validate(attrs)
-        
-        
